771af6ffe7cd6cbf2f1b7b31dd00156bfc920dd4
kylelee-dev <leekyle.dev@gmail.com>
2024-09-18 22:55:19 -0400

    handle signup

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,18 +1,40 @@
 import InputGroup from "@/components/InputGroup";
+import axios from "axios";
 import Link from "next/link";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
 
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
   const [errors, setErrors] = useState<any>({});
+
+  const router = useRouter();
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/auth/register", {
+        email,
+        password,
+        username,
+      });
+      console.log("res", res);
+    //   router.push("/login");
+    } catch (error: any) {
+      console.log("error:", error);
+      setErrors(error?.response?.data || {})
+    }
+
+  };
+
   return (
     <div className="bg-white">
       <div className="flex flex-col items-center justify-content h-screen p-6">
         <div className="w-10/12 mx-auto md:w-96">
           <h1 className="mb-2 text-lg font-medium">SIGN UP</h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <InputGroup
               placeholder="Email"
               value={email}
@@ -32,7 +54,10 @@ const Register = () => {
               setValue={setPassword}
               error={errors.password}
             />
-            <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+            <button
+              type="submit"
+              className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded"
+            >
               GO
             </button>
           </form>
