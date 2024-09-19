4d869537c9af8a93a1e45912a8eac9824a07edf9
kylelee-dev <leekyle.dev@gmail.com>
2024-09-18 22:59:39 -0400

    Create login page UI

---
+++ pages/login.tsx
@@ -0,0 +1,68 @@
+import InputGroup from '@/components/InputGroup'
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { FormEvent, useState } from 'react'
+
+const login = () => {
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [errors, setErrors] = useState<any>({});
+  
+    const router = useRouter();
+  
+    const handleSubmit = async (e: FormEvent) => {
+      e.preventDefault();
+      try {
+        const res = await axios.post("/auth/login", {
+          password,
+          username,
+        });
+        console.log("res", res);
+
+      } catch (error: any) {
+        console.log("error:", error);
+        setErrors(error?.response?.data || {})
+      }
+  
+    };
+  
+  return (
+    <div className="bg-white">
+      <div className="flex flex-col items-center justify-content h-screen p-6">
+        <div className="w-10/12 mx-auto md:w-96">
+          <h1 className="mb-2 text-lg font-medium">SIGN IN</h1>
+          <form onSubmit={handleSubmit}>
+
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
+            <button
+              type="submit"
+              className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded"
+            >
+              LOGIN
+            </button>
+          </form>
+          <small>
+            Don't have an account yet?
+            <Link href="/register" legacyBehavior>
+              <a className="ml-1 text-blue-500 uppercase">REGISTER</a>
+            </Link>
+          </small>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default login
