04251018b0b096da66ec49c18be2c978c1905393
kylelee-dev <leekyle.dev@gmail.com>
2024-09-25 12:42:23 -0400

    Add image upload for sub banner/image

--- pages/r/[sub].tsx
+++ pages/r/[sub].tsx
@@ -1,10 +1,14 @@
+import { useAuthState } from "@/context/auth";
 import axios from "axios";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import useSWR from "swr";
 
 const SubPage = () => {
+  const [ownSub, setOwnSub] = useState(false);
+
+  const { authenticated, user } = useAuthState();
   const fetcher = async (url: string) => {
     try {
       const res = await axios.get(url);
@@ -15,18 +19,52 @@ const SubPage = () => {
   };
   const router = useRouter();
   const subName = router.query.sub;
-
+  const fileInputRef = useRef<HTMLInputElement>(null);
   const { data: sub, error } = useSWR(
     subName ? `/subs/${subName}` : null,
     fetcher
   );
-  console.log(sub)
 
+  useEffect(() => {
+    if (!sub) return;
+    setOwnSub(authenticated && user!.username === sub.username);
+  }, [sub]);
+  const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
+    if(event.target.files === null) return;
+    
+    const file = event.target.files[0];
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("type", fileInputRef.current!.name);
+
+    try {
+        await axios.post(`/subs/${sub.name}/upload`, formData, {
+            headers: { "Content-Type": "multipart/form-data"}
+        })
+    } catch (error) {
+        console.log(error)
+    }
+  };
+  const openFileInput = (type: string) => {
+    const fileInput = fileInputRef.current;
+    if (fileInput) {
+      fileInput.name = type;
+      fileInput.click();
+    }
+  };
+  console.log(sub);
   return (
     <>
       {sub && (
         <>
           <div>
+            <input
+              type="file"
+              hidden={true}
+              ref={fileInputRef}
+              onChange={uploadImage}
+            />
             {/* Banner image */}
             <div className="bg-gray-400">
               {sub.bannerUrl ? (
@@ -38,12 +76,16 @@ const SubPage = () => {
                     backgroundSize: "cover",
                     backgroundPosition: "center",
                   }}
+                  onClick={() => openFileInput("banner")}
                 ></div>
               ) : (
-                <div className="h-20 bg-gray-400"></div>
+                <div
+                  className="h-20 bg-gray-400"
+                  onClick={() => openFileInput("banner")}
+                ></div>
               )}
             </div>
-            {/* Community metadata */}
+            {/* Community Metadata */}
             <div className="h-20 bg-white">
               <div className="relative flex max-w-5xl px-5 mx-auto">
                 <div className="absolute" style={{ top: -15 }}>
@@ -54,17 +96,16 @@ const SubPage = () => {
                       width={70}
                       height={70}
                       className="rounded-full"
+                      onClick={() => openFileInput("image")}
                     />
                   )}
                 </div>
                 <div className="pt-1 pl-24">
-                    <div className="flex items-center">
-                        <h1 className="mb-1 text-3xl font-bold">{sub.title}</h1>
-                    </div>
+                  <div className="flex items-center">
+                    <h1 className="mb-1 text-3xl font-bold">{sub.title}</h1>
+                  </div>
                 </div>
-                <p className="font-bold text-sm text-gray-400">
-                    /r/{sub.name}
-                </p>
+                <p className="font-bold text-sm text-gray-400">/r/{sub.name}</p>
               </div>
             </div>
           </div>
