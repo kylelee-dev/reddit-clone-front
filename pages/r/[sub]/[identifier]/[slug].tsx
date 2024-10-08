2d5beec6c560dfcfd5fa1557df33d970834ca457
kylelee-dev <leekyle.dev@gmail.com>
2024-10-08 00:32:37 -0400

    Refresh votes instantly by using mutate in the vote function

--- pages/r/[sub]/[identifier]/[slug].tsx
+++ pages/r/[sub]/[identifier]/[slug].tsx
@@ -23,11 +23,11 @@ const PostPage = () => {
     }
   };
 
-  const { data: post, error } = useSWR<Post>(
+  const { data: post, error, mutate: postMutate} = useSWR<Post>(
     identifier && slug ? `/posts/${identifier}/${slug}` : null,
     fetcher
   );
-  const { data: comments, mutate } = useSWR<Comment[]>(
+  const { data: comments, mutate: commentMutate } = useSWR<Comment[]>(
     identifier && slug ? `/posts/${identifier}/${slug}/comments` : null,
     fetcher
   );
@@ -42,8 +42,7 @@ const PostPage = () => {
       await axios.post(`/posts/${post?.identifier}/${post?.slug}/comment`, {
         body: newComment,
       });
-
-      mutate();
+      commentMutate();
       setNewComment("");
     } catch (error) {
       console.log(error);
@@ -66,6 +65,8 @@ const PostPage = () => {
         commentIdentifier: comment?.identifier,
         value,
       });
+      commentMutate();
+      postMutate();
     } catch (error) {
         console.log(error)
     }
